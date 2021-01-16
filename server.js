36ab4ff2a8dfe1db3596fae4b7e27e32efccda5e
Jongsun Park <jongsun250@gmail.com>
2021-01-16 14:05:55 +0000

    .

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ app.get("/api", (req, res, next) => {
   res.json({ message: "api connnected" });
 });
 
+app.get("/", (req, res, next) => {
+  res.json({ message: "it's root page" });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://127.0.0.1:${port}`);
 });
